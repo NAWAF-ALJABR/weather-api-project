5399003a988f18d8c52db5418a65263c785d109b
Nawaf Jabr Aljabr <Naljabr@sure.com.sa>
2026-05-24 10:47:56 +0300

    added weather api

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Weather API Project")
+import requests
+
+url = "https://api.open-meteo.com/v1/forecast?latitude=24.71&longitude=46.67&current=temperature_2m"
+
+response = requests.get(url)
+
+data = response.json()
+
+print("Temperature:", data["current"]["temperature_2m"], "°C")
